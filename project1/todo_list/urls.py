0f2aea2c22e96a3576cc025c9377f7117eeaa4be
yeerke <ezhumalin01@gmail.com>
2022-03-20 21:51:31 +0600

    first commit

--- project1/todo_list/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('main-page/', views.main_page)
-]
--- project1/todo_list/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Task
-
-def main_page(request):
-    tasks = Task.objects.all()
-    return render(request, 'main_page.html', {'tasks': tasks})
